d336b821f2c94dc5bc298a8a02a3978b63cdf6f1
Lucas <lucasdm0927@gmail.com>
2021-05-18 14:42:52 +0900

    setting static path

--- testPage/polls/urls.py
+++ testPage/polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import signupfunc
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', signupfunc, name='signup'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- testPage/testPage/settings.py
+++ testPage/testPage/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
