f136b0834f9fe6483cc3b85d95f738608582956e
Lucas <lucasdm0927@gmail.com>
2021-05-18 14:32:11 +0900

    add signup path in urlpatterns

--- testPage/polls/urls.py
+++ testPage/polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import signupfunc
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', signupfunc, name='signup'),
 ]
