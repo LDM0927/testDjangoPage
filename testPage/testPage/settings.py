042bf699e11d9a0d8642a956828bac0ec0532e7f
Lucas <lucasdm0927@gmail.com>
2021-05-18 15:01:16 +0900

    fix bug

--- testPage/testPage/settings.py
+++ testPage/testPage/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'testPage.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3'
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sample',
-        'USER': 'lucas',
-        'PASSWORD': 'Qwertyuiop',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'sample',
+        # 'USER': 'lucas',
+        # 'PASSWORD': 'Qwertyuiop',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
