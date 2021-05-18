28115ee9d7773e76cba95d123c795dbd00a6f0d5
Lucas <lucasdm0927@gmail.com>
2021-05-18 14:52:07 +0900

    define LOGIN_URL

--- testPage/testPage/settings.py
+++ testPage/testPage/settings.py
@@ -130,6 +130,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
