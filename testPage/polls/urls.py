382737549a421a44aab80b20c7c03025e5f02e2b
Lucas <lucasdm0927@gmail.com>
2021-05-18 14:56:30 +0900

    add userpagefunc path in urlpatterns

--- testPage/polls/urls.py
+++ testPage/polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signupfunc, loginfunc
+from .views import signupfunc, loginfunc, userpagefunc
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
+    path('userpage/', userpagefunc, name='userpage'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
