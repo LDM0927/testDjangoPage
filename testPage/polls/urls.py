110baad270803d19c37aef65dea89b056de38f0c
Lucas <lucasdm0927@gmail.com>
2021-05-18 20:16:14 +0900

    change description method

--- testPage/polls/urls.py
+++ testPage/polls/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import signupfunc, loginfunc, userpagefunc
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.indexfunc, name='index'),
-    path('signup/', signupfunc, name='signup'),
-    path('login/', loginfunc, name='login'),
-    path('userpage/', userpagefunc, name='userpage'),
+    path('signup/', views.signupfunc, name='signup'),
+    path('login/', views.loginfunc, name='login'),
+    path('userpage/', views.userpagefunc, name='userpage'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
